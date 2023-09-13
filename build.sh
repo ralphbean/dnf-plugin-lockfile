954afe08f5c3a2cea6fbc56b769748f5fd9c2b96
Ralph Bean <rbean@redhat.com>
2023-09-13 16:36:24 -0400

    Put tar contents inside a subdir

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 rm -rf ~/rpmbuild/SOURCES/*
 rm -rf ~/rpmbuild/SRPMS/*
-tar -czvf ~/rpmbuild/SOURCES/dnf-plugin-lockfile-1.0.tar.gz *
+tar -czvf ~/rpmbuild/SOURCES/dnf-plugin-lockfile-1.0.tar.gz --transform 's,^,dnf-plugin-lockfile-1.0/,' *
 rpmbuild -bs ./dnf-plugin-lockfile.spec
 copr build ralph/dnf-plugin-lockfile ~/rpmbuild/SRPMS/dnf-plugin-lockfile-1.0-1.fc38.src.rpm
