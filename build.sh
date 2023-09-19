939e9a3698cdeefd6cb0be2808983253914b0e30
Ralph Bean <rbean@redhat.com>
2023-09-19 14:37:21 -0400

    Bump spec release

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/bash -e
 
+old=$(grep Release: dnf-plugin-lockfile.spec| awk ' { print $2 } ')
+new=$(echo "$old"+1 | bc)
+sed -i "s/Release:        $old/Release:        $new/" ./dnf-plugin-lockfile.spec
+
+git commit -a -m "Bump release from $old to $new"
+
 rm -rf ~/rpmbuild/SOURCES/*
 rm -rf ~/rpmbuild/SRPMS/*
 tar -czvf ~/rpmbuild/SOURCES/dnf-plugin-lockfile-1.0.tar.gz --transform 's,^,dnf-plugin-lockfile-1.0/,' *
 rpmbuild -bs ./dnf-plugin-lockfile.spec
-copr build ralph/dnf-plugin-lockfile ~/rpmbuild/SRPMS/dnf-plugin-lockfile-1.0-1.fc38.src.rpm
+copr build ralph/dnf-plugin-lockfile ~/rpmbuild/SRPMS/dnf-plugin-lockfile-1.0-$new.src.rpm
